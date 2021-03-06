883c58030a82f3cfba3ad7a84f66011f7d455021
TDuong <tuanduong2920@gmail.com>
2021-03-06 22:22:34 +0700

    demo 1

--- app.js
+++ app.js
@@ -3,40 +3,69 @@ var express = require('express'),
     bodyParser = require('body-parser'),
     app = express();
 
-var myLimit = typeof (process.argv[2]) != 'undefined' ? process.argv[2] : '100kb';
+var myLimit = typeof(process.argv[2]) != 'undefined' ? process.argv[2] : '100kb';
 console.log('Using limit: ', myLimit);
 
-app.use(bodyParser.json({ limit: myLimit }));
+app.use(bodyParser.json({limit: myLimit}));
 
 
-app.all('*', function (req, res, next) {
-
+app.all('/stream', function (req, res, next) {
+    console.log('stream');
     // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
     res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
+    
 
     if (req.method === 'OPTIONS') {
         // CORS Preflight
         res.send();
     } else {
-
-        var targetURL = req.header('Target-URL'); // Target-URL ie. https://example.com or http://example.com
-
+        var targetURL = req.query.url; // Target-URL ie. https://example.com or http://example.com
         if (!targetURL) {
             res.send(500, { error: 'There is no Target-Endpoint header in the request' });
             return;
         }
-        request({ url: targetURL + req.url, method: req.method, json: req.body, headers: { 'Authorization': req.header('Authorization') } },
+        request({ url: targetURL + req.url, method: req.method, json: req.body, headers: {'Authorization': req.header('Authorization')} },
+            function (error, response, body) {
+                
+                if (error) {
+                    console.error('error: ' + response.statusCode)
+                }
+//                console.log(body);
+            }).pipe(res);
+    }
+});
+
+
+app.all('/search', function (req, res, next) {
+
+    // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
+    res.setHeader("Cache-Control",'no-cache, no-store, no-transform, max-age=0')
+    if (req.method === 'OPTIONS') {
+        // CORS Preflight
+        res.send();
+    } else {
+        // var targetURL = req.header('Target-URL'); // Target-URL ie. https://example.com or http://example.com
+        var targetURL = req.query.url;
+        // console.log(req.query)
+        if (!targetURL) {
+            res.send(500, { error: 'There is no Target-Endpoint header in the request' });
+            return;
+        }
+        request({ url: targetURL + req.url, method: req.method, json: req.body, headers: {'Authorization': req.header('Authorization')} },
             function (error, response, body) {
                 if (error) {
                     console.error('error: ' + response.statusCode)
                 }
-                //                console.log(body);
-            }).pipe(res);
 
+            }).pipe(res);
     }
 });
+
 
 app.set('port', process.env.PORT || 3001);
 
